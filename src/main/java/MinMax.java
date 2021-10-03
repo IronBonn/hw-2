a56f90595fe52cb5c9422df19c0f5c0b6f0f05c3
IronBonn <warprofe89@gmail.com>
2021-10-03 20:41:05 +0200

    first commit

---
+++ src/main/java/MinMax.java
@@ -0,0 +1,9 @@
+public class MinMax {
+    public static void main(String[] args) {
+
+        System.out.println(Integer.MIN_VALUE);
+        Integer a = Integer.MAX_VALUE;
+        System.out.println(++a);
+
+    }
+}
